3a278ae34c9c2c2c6bec1a35b1b72f24452999f5
AmrHani <117240613+Amr-Hani@users.noreply.github.com>
2024-07-20 18:11:36 +0300

    add class MyNav to navigate between screens

--- src/tic/toe/game/Mynav.java
+++ src/tic/toe/game/Mynav.java
@@ -1,5 +1,66 @@
 package tic.toe.game;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import screens.LoginScreen;
+
 public class Mynav {
+
+    static Scene scene;
+    static Stage stage;
+
+    public static void navigateTo(Parent destination, Scene currentScene) {
+        if (currentScene != null) {
+            currentScene.setRoot(destination);
+        } else {
+            System.err.println("Unable to navigate: current scene is null");
+        }
+    }
     
+    public static void navigateTo(Parent destinationRoot, Stage currentStage) {
+        if (destinationRoot != null && currentStage != null) {
+            Scene scene = new Scene(destinationRoot);
+            currentStage.setScene(scene);
+            currentStage.show();
+            currentStage.setResizable(false);
+        } else {
+            System.err.println("Unable to navigate: destinationRoot or currentStage is null");
+        }
+    }
+
+    public static void navigateTo(Parent distinationRoot, MouseEvent event) {
+        if (event != null && event.getSource() != null) {
+            scene = new Scene(distinationRoot);
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            showScene();
+        } else {
+            System.err.println("Unable to navigate: event or source is null");
+        }
+    }
+
+    public static void navigateTo(Parent distinationRoot, ActionEvent event) {
+        if (event != null && event.getSource() != null) {
+            scene = new Scene(distinationRoot);
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            showScene();
+        } else {
+            System.err.println("Unable to navigate: event or source is null");
+        }
+    }
+
+    private static void showScene() {
+        if (stage != null && scene != null) {
+            stage.setScene(scene);
+            stage.show();
+            stage.setResizable(false);
+        } else {
+            System.err.println("Unable to show scene: stage or scene is null");
+        }
+    }
+
 }
